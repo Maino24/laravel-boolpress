6abf7dba7e557fa863829206b1ad57a242b362b3
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-02-04 10:20:46 +0100

    Route tags

--- routes/api.php
+++ routes/api.php
@@ -26,4 +26,11 @@ Route::namespace('Api')->prefix('/posts')->group(function(){
     Route::get('/', 'PostController@index');
     //localhost:8000/api/posts/12
     Route::get('/{id}', 'PostController@show');
+
+});
+
+
+Route::namespace('Api')->prefix('/tags')->group(function(){
+    //localhost:8000/api/tags
+    Route::get('/', 'TagsController@index');
 });
